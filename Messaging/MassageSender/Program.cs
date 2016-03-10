132584d0b967b7b2c46db14877ca03ca324d0be5
larssherman <larssherman@gmail.com>
2016-03-10 20:32:37 +0100

    Message sender added

---
+++ Messaging/MassageSender/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassageSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MessageSender started...");
+
+            Console.ReadKey();
+        }
+    }
+}
